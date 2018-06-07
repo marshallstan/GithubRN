54e3aa6e0f7883b748ef053fd9c56e806f971079
marshall wu <marshall.wu@cssinco.cn>
2018-06-07 10:57:12 +0800

    console.ignoredYellowBox = ['Remote debugger']

--- index.js
+++ index.js
@@ -3,5 +3,6 @@ import App from './App';
 
 import { YellowBox } from 'react-native';
 YellowBox.ignoreWarnings(['Warning: isMounted(...) is deprecated', 'Module RCTImageLoader']);
+console.ignoredYellowBox = ['Remote debugger'];
 
 AppRegistry.registerComponent('testApp', () => App);
